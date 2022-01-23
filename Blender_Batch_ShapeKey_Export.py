cde561a63fbfe7c887726006ff11121b1c0afa80
3DSphinx <symickens@gmail.com>
2022-01-23 00:28:17 -0500

    Update Blender_Batch_ShapeKey_Export.py
    Notice error at times for exporting so adjust how to get the directory of the save blend file.

--- Blender_Batch_ShapeKey_Export.py
+++ Blender_Batch_ShapeKey_Export.py
@@ -9,16 +9,23 @@ import os
 ##get the selected object
 try:
     exp_Object=str(bpy.context.selected_objects[0].data.name)
+    ##get the blend file save location and create a folder
+    baseName=bpy.path.basename(bpy.data.filepath)
+    baseNameSize=len(baseName)
+    fileNameSize=len(bpy.data.filepath)
+    save_Loc=bpy.data.filepath[0:fileNameSize-baseNameSize]
     
     newDirName="Exported_ShapeKeys";
+    
+    
     ##Let's make sure object is selected
     if(exp_Object!=None):
         ##get the blend file save location and create a folder
-        save_Loc=os.getcwd()
+        ##save_Loc=os.getcwd()
         
         ##make a new directory
         try:
-            os.mkdir(newDirName)
+            os.mkdir(save_Loc+"/"+newDirName)
         except FileExistsError:
             print("Directory already exist skipping creation.")
             
@@ -35,7 +42,7 @@ try:
             for shape in range(1,loop):
                 ##this will set the value and then export out
                 bpy.data.meshes[exp_Object].shape_keys.key_blocks[shape].value=1;
-                bpy.ops.export_scene.obj(filepath=save_Loc+"//"+newDirName+"//"+bpy.data.meshes[exp_Object].shape_keys.key_blocks[shape].name+".obj",check_existing=False,use_selection=True,use_materials=False,use_vertex_groups=True,keep_vertex_order=True)
+                bpy.ops.export_scene.obj(filepath=save_Loc+"/"+newDirName+"/"+bpy.data.meshes[exp_Object].shape_keys.key_blocks[shape].name+".obj",check_existing=False,use_selection=True,use_materials=False,use_vertex_groups=True,keep_vertex_order=True)
                 ##reset shapekey
                 bpy.data.meshes[exp_Object].shape_keys.key_blocks[shape].value=0
         else:
